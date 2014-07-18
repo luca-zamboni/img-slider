b9f7e3befb6377246431716d98f02ae7280decd9
luca-zamboni <lucazamboni92@gmail.com>
2014-07-18 23:32:30 +0200

    Dovrebbe funziare anche il resize

--- img-slider/img-slider.js
+++ img-slider/img-slider.js
@@ -1,158 +1,183 @@
 
 jQuery.fn.imgSlider = function(options){
 
-	var fade = 1000;
+	var current = this;
+	var resize;
+	var ctrlResize;
 
-	var a,neww,newh,grapp,ropp,part,gh,gw,arr = 0,i=0,lobj,j;
+	$(current).css("overflow","hidden");
+	$(current).css("position","relative");
 
-	gw = $(this).parent().width();
-	gh = $(this).parent().height();
+	gooo();
 
-	$(this).css("width",gw+"px");
-	$(this).css("height",gh+"px");
-	grapp = gw/gh;
+	function gooo(){
+		resize = false;
+		var fade = 1000;
 
-	a = $(this).children();
-	a = Array.prototype.slice.call(a);
+		var a,neww,newh,grapp,ropp,part,gh,gw,arr = 0,i=0,lobj,j;
 
-	a.reverse().forEach(function(e){
-		$(e).on('load', function(){
-			setta(e);
+		gw = $(current).width();
+		gh = $(current).height();
+			console.log(gw);
+
+		/*$(current).css("width",gw+"px");
+		$(current).css("height",gh+"px");*/
+		grapp = gw/gh;
+
+		a = $(current).children();
+		a = Array.prototype.slice.call(a);
+
+		a.reverse().forEach(function(e){
+			$(e).on('load', function(){
+				setta(e);
+			});
+			$(e).fadeOut(0);
 		});
-		$(e).fadeOut(0);
-	});
-	a.reverse();
-
-	obj = a[i];
-	i++;
-	j=false;
-
-	$(obj).fadeIn(0,function(){
-		setTimeout(sposta,getInitStop(obj));
-	});
-
-	function anima(){
-
-		if(j){
-			i++;
-			if(i>=a.length){
-			    i=0;
-			}
-			lobj = obj;
-		}
-		j = true;
+		a.reverse();
 
 		obj = a[i];
+		i++;
+		j=false;
 
-		setta(obj);
-
-		$(lobj).fadeOut(fade);
-		$(obj).fadeIn(fade/2,function(){
+		$(obj).fadeIn(0,function(){
 			setTimeout(sposta,getInitStop(obj));
 		});
-		
-		
-	}
 
-	function sposta(){
-		if(obj.dire){
-		    $(obj).animate({
-		        marginTop : obj.arr + "px"
-		    }, obj.speed,function(){
-		    	setTimeout(anima,getStop(obj));
-		    });
-		}else{
-		    $(obj).animate({
-		        marginLeft : obj.arr + "px"
-		    }, obj.speed,function(){
-		    	setTimeout(anima,getStop(obj));
-		    });
-		}
-	}
+		$(window).resize(function(){
+			gw = $(current).width();
+			gh = $(current).height();
 
-	function setta(id){
-		w = $(id).width();
-		h = $(id).height();
-		rapp = w/h;
-		id.dire = rapp < grapp;
-		if(id.dire){
-			ropp = gw / w;
-			neww = gw;
-			newh = h * ropp;
-			part = (gh-newh);
-			if(getDir(id)){
-				$(id).css("marginTop",part+"px");
-				id.arr = 0;
-			}else{
-				id.arr = part
-				$(id).css("marginTop",0 + "px");
+			/*$(current).css("width",gw+"px");
+			$(current).css("height",gh+"px");*/
+			grapp = gw/gh;
+
+			a.forEach(function(e){
+				setta(e);
+			});
+		});
+
+		function anima(){
+			if(j){
+				i++;
+				if(i>=a.length){
+					i=0;
+				}
+				lobj = obj;
 			}
-		}else{
-			ropp = gh / h;
-			newh = gh;
-			neww = w * ropp;
-			part = (gw-neww);
-			if(getDir(id)){
-		    	$(id).css("marginLeft",part+"px");
-		    	id.arr = 0;
+			j = true;
+
+			obj = a[i];
+
+			setta(obj);
+
+			$(lobj).fadeOut(fade);
+			$(obj).fadeIn(fade/2,function(){
+				setTimeout(sposta,getInitStop(obj));
+			});
+
+
+		}
+
+		function sposta(){
+			if(obj.dire){
+				$(obj).animate({
+					marginTop : obj.arr + "px"
+				}, obj.speed,function(){
+					setTimeout(anima,getStop(obj));
+				});
 			}else{
-				id.arr = part
-				$(id).css("marginLeft",0 + "px");
+				$(obj).animate({
+					marginLeft : obj.arr + "px"
+				}, obj.speed,function(){
+					setTimeout(anima,getStop(obj));
+				});
 			}
 		}
-		$(id).css("width",neww+"px");
-		$(id).css("height",newh+"px");
-		id.speed = getSpeed(id,w,h);
-	}
 
-	function getInitStop(id){
-		istop = 1000*getNumFromClass(id,"i-stop");
-		if(istop < 0 || isNaN(istop)){
-			istop = 1000;
-		}
-		return istop;
-	}
-
-	function getStop(id){
-		stop = 1000*getNumFromClass(id,"stop");
-		if(stop < 0 || isNaN(stop)){
-			stop = 1000;
-		}
-		return stop;
-	}
-
-	function getSpeed(id){
-		speed = 1000*getNumFromClass(id,"speed");
-		if(speed < 0 || isNaN(speed)){
+		function setta(id){
+			w = $(id).width();
+			h = $(id).height();
+			rapp = w/h;
+			id.dire = rapp < grapp;
 			if(id.dire){
-				speed = ( newh / gh ) * 2000;
+				ropp = gw / w;
+				neww = gw;
+				newh = h * ropp;
+				part = (gh-newh);
+				if(getDir(id)){
+					$(id).css("marginTop",part+"px");
+					id.arr = 0;
+				}else{
+					id.arr = part
+					$(id).css("marginTop",0 + "px");
+				}
 			}else{
-				speed = ( neww / gw ) * 2000;
+				ropp = gh / h;
+				newh = gh;
+				neww = w * ropp;
+				part = (gw-neww);
+				if(getDir(id)){
+					$(id).css("marginLeft",part+"px");
+					id.arr = 0;
+				}else{
+					id.arr = part
+					$(id).css("marginLeft",0 + "px");
+				}
 			}
+			$(id).css("width",neww+"px");
+			$(id).css("height",newh+"px");
+			id.speed = getSpeed(id,w,h);
 		}
-		return speed;
-	}
 
-	function getDir(id){
-		ret = true;
-		$(id).attr("class").split(" ").forEach(function(e){
-			if(e == "inv-dir"){
-				ret = false;
+		function getInitStop(id){
+			istop = 1000*getNumFromClass(id,"i-stop");
+			if(istop < 0 || isNaN(istop)){
+				istop = 1000;
 			}
-		});
-		return ret;
-	}
+			return istop;
+		}
 
-	function getNumFromClass(id,sub){
-		num = -1;
-		classes = $(id).attr("class");
-		split = classes.split(" ");
-		split.forEach(function(e){
-			if(e.indexOf(sub) != -1){
-				num = parseInt(e.substring(sub.length,e.length));
+		function getStop(id){
+			stop = 1000*getNumFromClass(id,"stop");
+			if(stop < 0 || isNaN(stop)){
+				stop = 1000;
 			}
-		});
-		return num;
+			return stop;
+		}
+
+		function getSpeed(id){
+			speed = 1000*getNumFromClass(id,"speed");
+			if(speed < 0 || isNaN(speed)){
+				if(id.dire){
+					speed = ( newh / gh ) * 2000;
+				}else{
+					speed = ( neww / gw ) * 2000;
+				}
+			}
+			return speed;
+		}
+
+		function getDir(id){
+			ret = true;
+			$(id).attr("class").split(" ").forEach(function(e){
+				if(e == "inv-dir"){
+					ret = false;
+				}
+			});
+			return ret;
+		}
+
+		function getNumFromClass(id,sub){
+			num = -1;
+			classes = $(id).attr("class");
+			split = classes.split(" ");
+			split.forEach(function(e){
+				if(e.indexOf(sub) != -1){
+					num = parseInt(e.substring(sub.length,e.length));
+				}
+			});
+			return num;
+		}
 	}
 }
 
